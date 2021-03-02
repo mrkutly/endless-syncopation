2030f17a19bc17eb33e2d890cecf487e54fb014f
mark sauer-utley <mark.sauerutley@respondent.io>
2021-03-01 20:53:32 -0400

    put eslintconfig in reasonable place

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+	parser: 'babel-eslint',
+	parserOptions: {
+		sourceType: 'module',
+		allowImportExportEverywhere: true,
+	},
+	env: {
+		'jest/globals': true,
+		browser: true,
+	},
+	plugins: [
+		'jest',
+	],
+	extends: [
+		'airbnb',
+	],
+	rules: {
+		'react/prop-types': 'off',
+		'no-underscore-dangle': 0,
+		'no-unused-vars': 1,
+		'no-sequences': 1,
+		'react/react-in-jsx-scope': 0,
+		'react/jsx-indent': [
+			2,
+			'tab',
+		],
+		'react/jsx-indent-props': [
+			2,
+			'tab',
+		],
+		'max-len': [
+			'error',
+			120,
+		],
+		semi: [
+			2,
+			'never',
+		],
+		indent: [
+			2,
+			'tab',
+		],
+		'no-tabs': 'off',
+	},
+}
