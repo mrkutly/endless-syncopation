3f1ca585f95e0f838e2f9427ba95dcaeea6a03c9
mark sauer-utley <mark.sauerutley@respondent.io>
2021-03-03 09:31:08 -0400

    remove comments

--- src/lib/metronome.js
+++ src/lib/metronome.js
@@ -1,16 +1,15 @@
+/* eslint-disable no-alert */
 import { identity } from 'ramda'
 import { tryCatch } from './utils'
 
 class Metronome {
 	constructor(tempo = 115) {
 		this.audioContext = null
-		// notes that have been put into the web audio and may or may not have been played yet {note, time}
-		this.notesInQueue = []
 		this.currentQuarterNote = 0
 		this.tempo = tempo
-		this.lookahead = 25 // How frequently to call scheduling function (in milliseconds)
-		this.scheduleAheadTime = 0.1 // How far ahead to schedule audio (sec)
-		this.nextNoteTime = 0.0 // when the next note is due
+		this.lookahead = 25
+		this.scheduleAheadTime = 0.1
+		this.nextNoteTime = 0.0
 		this.isRunning = false
 		this.interval = null
 	}
@@ -27,13 +26,8 @@ class Metronome {
 	}
 
 	nextNote() {
-		// Advance current note and time by a quarter note
-		// Notice this picks up the CURRENT tempo value to calculate beat length.
 		const secondsPerBeat = 60.0 / this.tempo
-		// Add beat length to last beat time
 		this.nextNoteTime += secondsPerBeat
-
-		// Advance the beat number, wrap to zero
 		this.currentQuarterNote += 1
 		if (this.currentQuarterNote === 4) {
 			this.currentQuarterNote = 0
@@ -41,10 +35,6 @@ class Metronome {
 	}
 
 	scheduleNote(beatNumber, time) {
-		// push the note on the queue, even if we're not playing.
-		this.notesInQueue.push({ note: beatNumber, time })
-
-		// create an oscillator
 		const osc = this.audioContext.createOscillator()
 		const envelope = this.audioContext.createGain()
 
@@ -61,7 +51,6 @@ class Metronome {
 	}
 
 	scheduler() {
-		// while there are notes that will need to play before the next interval, schedule them and advance the pointer.
 		while (this.nextNoteTime < this.audioContext.currentTime + this.scheduleAheadTime) {
 			this.scheduleNote(this.currentQuarterNote, this.nextNoteTime)
 			this.nextNote()
